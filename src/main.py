2ecb08bc1773a394c902432514fc17ff02bb4849
leonel vinasco <debi@MacBook-Air-de-leonel.local>
2023-03-22 02:08:28 -0500

    testing get endpoints

--- src/main.py
+++ src/main.py
@@ -8,10 +8,32 @@ app = FastAPI()
 
 @app.get("/v1/topics")
 def read_root():
-    result = subprocess.run(["sh","/usr/local/kafka/bin/kafka-configs.sh", "--bootstrap-server", "kafkaBroker1:9077", "--all" ,"--entity-type" ,"brokers", "--entity-name", "1", "--describe"], check=True, stdout=subprocess.PIPE, universal_newlines=True)
-    
-    return {"Hello": "World", "result":result.stdout}
+    try:
+        result = subprocess.run(["sh","/usr/local/kafka/bin/kafka-configs.sh", "--bootstrap-server", "kafkaBroker1:9077", "--all" ,"--entity-type" ,"brokers", "--entity-name", "1", "--describe"], 
+                            check=True, stdout=subprocess.PIPE,stderr=subprocess.PIPE, universal_newlines=True)
+    except:
+        print("An exception ocurred")
+    return {"returnCode": result.returncode if result != None else "NULL", "result":result.stdout, "error":result.stderr}
 
+@app.get("/v1/topics1")
+def read_root():
+    try:
+        result = subprocess.run(["sh","/usr/local/kafka/bin/kafka-configs.sh", "--bootstrap-server", "kafkaBroker1:9077", "--all" ,"--entity-type" ,"brokers", "--entity-name", "2", "--describe"], 
+                            check=True, stdout=subprocess.PIPE,stderr=subprocess.PIPE, universal_newlines=True)
+    except:
+        print("An exception ocurred")
+    return {"returnCode": result.returncode if result != None else "NULL", "result":result.stdout, "error":result.stderr}
+#Validaciones extremas con entrada, para no frenar el subprocess. 
+# Por ejemplo verificar siexiste el broker con id4 antes del subprocess
+@app.get("/v1/topics2")
+def read_root():
+    result = None
+    try:
+        result = subprocess.run(["sh","/usr/local/kafka/bin/kafka-configs.sh", "--bootstrap-server", "kafkaBroker1:9077", "--all" ,"--entity-type" ,"brokers", "--entity-name", "4", "--describe"], 
+                            check=True, stdout=subprocess.PIPE,stderr=subprocess.PIPE, universal_newlines=True)
+    except:
+        print("An exception ocurred")
+    return {"returnCode": result.returncode if result != None else "NULL", "result":result.stdout, "error":result.stderr}
 
 #subprocess.Popen(f"curl --location 'http://localhost:8000/v1/topics/'", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
 #command = "echo prueba"
